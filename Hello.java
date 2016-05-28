067c9f7b97c6af03905759c6e9f1530c5cecdb98
ratsrus12 <srujithchowdary@gmail.com>
2016-05-28 12:53:21 +0530

    java sample

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+	public static void main(String args[])
+		{
+			String name = "srujith";
+			int age = 25;
+			System.out.println("hello " + name);
+			System.out.println("You're already " +age+ ",let's see if you can hit 30");
+		}
+}
